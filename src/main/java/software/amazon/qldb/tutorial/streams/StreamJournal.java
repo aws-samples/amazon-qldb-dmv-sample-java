13786f86ca97a9835af4188a80974912c44468e2
Saurabh Mehta <saumehta@amazon.com>
2020-05-18 16:10:48 -0700

    Fix license header
    Drop year from license header.
    Remove * import from Verifier.java

--- src/main/java/software/amazon/qldb/tutorial/streams/StreamJournal.java
+++ src/main/java/software/amazon/qldb/tutorial/streams/StreamJournal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: MIT-0
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
